20d5fbd962aaf770e1ec83b67b27799c59b1027d
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2022-03-17 02:29:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md', 'r', encoding='utf-8').read()
 
 setup(
     name="camptools",
-    version="0.6.4",
+    version="0.6.5",
     install_requires=[
         'f90nml',
     ],
