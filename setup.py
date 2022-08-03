6289855e76330c029164eaf21f1fcfb56a536a4e
t23545 <t23545@grand1.hucc>
2022-08-03 18:49:24 +0900

    Add: Added __init__.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md', 'r', encoding='utf-8').read()
 
 setup(
     name="camptools",
-    version="0.8.1",
+    version="0.8.2",
     install_requires=[
         'f90nml',
     ],
