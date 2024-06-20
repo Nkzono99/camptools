f1524b8f81b599ba7c58ca1869b1d34cba8f5d0e
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2024-06-21 00:56:05 +0900

    Added the function to allow macro-like definitions in inp files.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
             'nmysbatch = camptools.job_scheduler.sbatch_wrapper:nmysbatch',
             'mysbatch = camptools.job_scheduler.sbatch_wrapper:mysbatch',
             'latestjob = camptools.latest_job: main',
+            'preinp = camptools.preinp: main'
         ]
     },
     author="Nkzono",
@@ -44,4 +45,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    include_package_data=True,
 )
