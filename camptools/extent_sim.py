9542545067e5f1d72ffab6b33ede9b33ccc61c53
Nkzono99 <210x218x@stu.kobe-u.ac.jp>
2025-07-16 16:20:58 +0900

    Add: Added selecting queue option into extentsim command.

--- camptools/extent_sim.py
+++ camptools/extent_sim.py
@@ -10,12 +10,14 @@ from .utils import call, copy, symlinkdir
 def parse_args():
     parser = ArgumentParser()
 
-    parser.add_argument('from_directory')
-    parser.add_argument('to_directory', nargs='?', default=None)
-    parser.add_argument('--nstep', '-n', type=int, default=None)
-    parser.add_argument('--small', '-small', action='store_true')
-    parser.add_argument('--run', action='store_true')
-    parser.add_argument('--submit', '-s', default='mysbatch')
+    parser.add_argument("from_directory")
+    parser.add_argument("to_directory", nargs="?", default=None)
+    parser.add_argument("--nstep", "-n", type=int, default=None)
+    parser.add_argument("--small", "-small", action="store_true")
+    parser.add_argument("--run", action="store_true")
+    parser.add_argument("--submit", "-s", default="mysbatch")
+    parser.add_argument("--queue_name", "-qn", default=None)
+    parser.add_argument("--afterok", type=int, default=None)
 
     return parser.parse_args()
 
@@ -31,12 +33,12 @@ def extent_sim():
         to_dir = Path(args.to_directory)
     else:
         index = 2
-        to_dir = Path(f'{args.from_directory}_{index}')
+        to_dir = Path(f"{args.from_directory}_{index}")
         while to_dir.exists():
             from_dir = to_dir
 
             index += 1
-            to_dir = Path(f'{args.from_directory}_{index}')      
+            to_dir = Path(f"{args.from_directory}_{index}")
 
     to_dir.mkdir(exist_ok=True)
 
@@ -49,29 +51,42 @@ def extent_sim():
 
     # 最低限の要素の複製を行う場合以下は無視する
     if not args.small:
-        copy(from_dir / 'job.sh', to_dir / 'job.sh')
-        copy(from_dir / 'mpiemses3D', to_dir / 'mpiemses3D')
-        copy(from_dir / 'generate_xdmf3.py', to_dir / 'generate_xdmf3.py')
+        copy(from_dir / "job.sh", to_dir / "job.sh")
+        copy(from_dir / "mpiemses3D", to_dir / "mpiemses3D")
+        copy(from_dir / "generate_xdmf3.py", to_dir / "generate_xdmf3.py")
 
-        for bash in from_dir.glob('*.sh'):
+        for bash in from_dir.glob("*.sh"):
             copy(bash, to_dir)
-        for inp_file in from_dir.glob('*.inp'):
+        for inp_file in from_dir.glob("*.inp"):
             copy(inp_file, to_dir)
-        
-    if (from_dir / 'plasma.preinp').exists():
-        with open(from_dir / 'plasma.preinp', 'r', encoding='utf-8') as f:
+
+    if (from_dir / "plasma.preinp").exists():
+        with open(from_dir / "plasma.preinp", "r", encoding="utf-8") as f:
             lines = f.readlines()
-    
-        with open(to_dir / 'plasma.preinp', 'w', encoding='utf-8') as f:
+
+        with open(to_dir / "plasma.preinp", "w", encoding="utf-8") as f:
             for line in lines:
-                line = re.sub(r'jobnum(\(.*\))? *= *0 *, *[01]', 'jobnum(1:2) = 1, 1', line)
+                line = re.sub(
+                    r"jobnum(\(.*\))? *= *0 *, *[01]", "jobnum(1:2) = 1, 1", line
+                )
                 if args.nstep:
-                    line = re.sub(r'nstep *= *[0-9]+', f'nstep = {args.nstep}', line)
+                    line = re.sub(r"nstep *= *[0-9]+", f"nstep = {args.nstep}", line)
                 f.write(line)
     else:
-        inp.save(to_dir / 'plasma.inp')
-    symlinkdir((from_dir / 'SNAPSHOT1').resolve(), to_dir / 'SNAPSHOT0')
+        inp.save(to_dir / "plasma.inp")
+    symlinkdir((from_dir / "SNAPSHOT1").resolve(), to_dir / "SNAPSHOT0")
 
     if args.run:
         os.chdir(to_dir.resolve())
-        call(f'{args.submit} job.sh')
+
+        parts = [args.submit, "job.sh"]
+
+        if args.queue_name:
+            parts += ["-qn", args.queue_name]
+
+        if args.afterok:
+            parts += ["--afterok", args.afterok]
+
+        cmd = " ".join(parts)
+
+        call(cmd)
